c0cccacbe7a04ab8a182a044fee66a8a2b0cc3f2
Maharshii Banerjee <62034354+maharshiibanerjee@users.noreply.github.com>
2022-03-03 15:36:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Good Morning Jenkins!"); 
+        System.out.println("Good Morning Jenkins!");
+        System.out.println("Hello!! from RIshi");
     }
 }
